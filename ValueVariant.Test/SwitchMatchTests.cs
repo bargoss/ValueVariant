40daa61b71bf9af3eb3c53193ddf0809a697450f
Baran Yesiltepe <byesiltepe@zynga.com>
2023-08-11 10:59:57 +0300

    Switch and match statements code generation

---
+++ ValueVariant.Test/SwitchMatchTests.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace ValueVariant.Test
+{
+    public class SwitchMatchTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            TestVariantBargos a  = 1;
+            a.Switch(i =>
+            {
+
+            }, l =>
+            {
+
+            }, b =>
+            {
+
+            });
+        }
+    }
+
+    [ValueVariant]
+    public readonly partial struct TestVariantBargos : IValueVariant<TestVariantBargos, int, long, bool>
+    {
+        
+    }
+}
