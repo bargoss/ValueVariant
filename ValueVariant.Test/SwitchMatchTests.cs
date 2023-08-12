547da622d0935909a24911354aef168b134fcace
Baran Yesiltepe <byesiltepe@zynga.com>
2023-08-12 13:48:01 +0300

    autocompletions with type names

--- ValueVariant.Test/SwitchMatchTests.cs
+++ ValueVariant.Test/SwitchMatchTests.cs
@@ -8,22 +8,29 @@ namespace ValueVariant.Test
         public void Test1()
         {
             TestVariantBargos a  = 1;
-            a.Switch(i =>
-            {
-
-            }, l =>
-            {
-
-            }, b =>
-            {
-
-            });
+            a.Switch(
+                i => { },
+                command => { },
+                bargos => { },
+                b => { }
+            );
         }
     }
 
     [ValueVariant]
-    public readonly partial struct TestVariantBargos : IValueVariant<TestVariantBargos, int, long, bool>
+    public readonly partial struct TestVariantBargos : IValueVariant<TestVariantBargos, int, PlayerCommand, Bargos, bool>
     {
         
+    }
+
+    public struct PlayerCommand
+    {
+        public int A;
+        public int B;
+    }
+    public struct Bargos
+    {
+        public double A;
+        public float B;
     }
 }
